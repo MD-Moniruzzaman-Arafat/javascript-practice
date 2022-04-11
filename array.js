25880eb39679d4121332b764bdeeac38ccab2086
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-04-11 16:23:18 +0600

    array

---
+++ array.js
@@ -0,0 +1,15 @@
+var myArray = [1, 2, 3];
+var theSameArray = new Array(1, 2, 3);
+
+console.log(myArray);
+console.log(theSameArray);
+
+console.log(myArray[1]);      // prints out 2
+
+var myArray = []
+myArray[3] = "hello"
+console.log(myArray);
+
+var myArray = ["string", 10, {}]
+console.log(myArray[2]);
+console.log(typeof myArray[2]);
