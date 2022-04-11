a045f5bafcd01277edf319c86d9225c29c7a56d2
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-04-11 16:49:36 +0600

    operators

---
+++ operators.js
@@ -0,0 +1,16 @@
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);          // c is now equal to 3
+
+var name = "John";
+console.log("Hello " + name + "!");
+console.log("The meaning of life is " + 42);
+console.log(42 + " is the meaning of life");
+
+console.log(1 + "1");   // outputs "11"
+
+console.log(3 - 5);     // outputs -2
+console.log(3 * 5);     // outputs 15
+console.log(3 / 5);     // outputs 0.6
+console.log(5 % 3);     // outputs 2
