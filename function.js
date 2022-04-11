070442901c6ed0d9451d91fd0db4874a36437817
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-04-11 23:43:00 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    return "Hello " + name + "!";
+}
+
+console.log(greet("Eric"));      // prints out Hello Eric!
+
+var greet = function (name) {
+    return "Hello " + name + "!";
+}
+
+console.log(greet("Eric"));      // prints out Hello Eric!
