ed2464643a788b71cf92aaf4e923b4734571a2cd
MD Moniruzzaman Arafat <arafatmonir091@gmail.com>
2022-04-11 15:25:31 +0600

    helloWorld,variableAndDataType

---
+++ variableAndDataType.js
@@ -0,0 +1,24 @@
+var myNumber = 3;                   // a number
+var myString = "Hello, World!"      // a string
+var myBoolean = true;               // a boolean
+var myArray = [];                    // an array
+var myObject = {};                  // an object
+
+console.log(myNumber);
+console.log(myString);
+console.log(myBoolean);
+console.log(myArray);
+console.log(myObject);
+
+// data type
+console.log(typeof myNumber);
+console.log(typeof myString);
+console.log(typeof myBoolean);
+console.log(typeof myArray);
+console.log(typeof myObject);
+
+var newVariable;
+console.log(newVariable); //prints undefined
+
+var emptyVariable = null;
+console.log(emptyVariable);
